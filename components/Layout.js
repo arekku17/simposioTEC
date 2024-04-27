574e346358618774630d28ab9e45e156cb3e71c2
Arekku <jose_alex170702@hotmail.com>
2024-04-27 11:34:10 -0600

    ser patrocinador button

--- components/Layout.js
+++ components/Layout.js
@@ -65,9 +65,13 @@ export default function Main() {
               <p>REGISTRARSE AHORA</p>
             </button>
 
-            <button className="sponsorBtn inline-flex h-12 md:h-16 items-center justify-center p-0.5 mb-2 me-2 overflow-hidden font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <a 
+            target="_blank"
+            href="https://forms.gle/jbjrmghdLyPocvAT8"
+            className="sponsorBtn inline-flex h-12 md:h-16 items-center justify-center p-0.5 mb-2 me-2 overflow-hidden font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+              
               <p className="px-10 py-2.5 transition-all ease-in duration-75  dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full h-full flex justify-center items-center">SER PATROCINADOR</p>
-            </button>
+            </a>
           </div>
 
         </section>
